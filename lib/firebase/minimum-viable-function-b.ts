87888e20696e6b7b1d9abeecb12c2105aea6e41b
ikhsan-worker <ikhsan.mahendri@tiket.com>
2022-12-29 17:08:05 +0700

    feat: [REMOVE] remove file unused

--- lib/firebase/minimum-viable-function-b.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { https } = require('firebase-functions');
-
-const sendEmail = https.onRequest((req: any, res: any) => {
-  res.send({ status: 200 });
-});
-
-module.exports = sendEmail;
